c2c363dbf189c07b38cec5b9cca077659e43d15e
Ben Tristem <github@bentristem.com>
2018-03-16 11:46:02 +0000

    Going for simulated movement
    + as opposed to "kinematic" or non-simulated
    + setting velocity from controls
    + drag and shit sucks

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    [SerializeField] float speed = 5f;
+    Rigidbody2D playerRigidBody;
+
 	// Use this for initialization
 	void Start () {
-		
+        playerRigidBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        print(CrossPlatformInputManager.GetAxis("Horizontal"));
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value between -1 and +1
+        print(controlThrow);
 
         if (CrossPlatformInputManager.GetButton("Horizontal")) // reports every frame
         {
-            print("One of the Horizontal inputs pressed");
-
+            Vector2 playerVelocity = new Vector2(controlThrow * speed, 0f);
+            playerRigidBody.velocity = playerVelocity;
         }
 	}
 }
